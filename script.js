4fd436ba71037a0abdd083169e9b7529c89833df
Vandice Tech <amahlephumla80@gmail.com>
2026-06-22 15:12:39 +0200

    ciommit

--- script.js
+++ script.js
@@ -1,103 +1,59 @@
 const cells = document.querySelectorAll(".cell");
 const statusText = document.getElementById("status");
+const restartBtn = document.getElementById("restartBtn");
 
 let board = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer ="X";
 let gameActive = true;
 
 
-const winConditions = [
+const winPatterns = [
 [0,1,2], [3,4,5], [6,7,8],
 [0,3,6], [1,4,7], [2,5,8],
 [0,4,8], [2,4,6]
 ];
 
-cells.forEach((cell => {
-    cell.addEventListener("click",handleClick);
+cells.forEach(cell => {
+    cell.addEventListener("click", cellClicked);
 });
 
-function handleClick() {
-       if(this.textContent !== "" || !gameActive) {
-        return;
-       }
-    
-       this.textContent = currentPlayer;
+restartBtn.addEventListener("click", restartGame);
 
-        if(checkWinner()) {
-            
-        }
-        
-        if (gameActive) {
-            statusText.textContent = "Computer is thinking...";
-            setTimeout(() => {
-                computerMove();
-         }, 500);
-        }     
-    });
-});
+function cellClicked() {
+    const index = this.dataset.index;
 
+    if(board[index] !== "" || !gameActive) return;
 
-function makeMove(index, player) {
-    board[index] = player;
-    cells[index].textContent = player;
+    board[index] = currentPlayer;
+    this.textContent = currentPlayer;
 
-    if (player === "X") {
-        cells[index].classList.add("x");
+    if(currentPlayer === "X") {
+        this.classList.add("x");
     } else {
-        cells[index].classList.add("o");
-    }
-
-    checkWinner();
-
-    if(!gameActive) return;
-
-    currentPlayer = player === "X" ? "O" : "X";
-    statusText.textContent = `Player ${currentPlayer}'s turn`;
-}
-
-function comptureMove() {
-    let emptyCells =[];
-
-     for (let i = O; i < board.length; i++) {
-        if (board[i] === "") {
-            emptyCells.push(i);
-        }
-     }
-
-    if (emptyCells.length === O || !gameActive) 
-        return; 
-
-    let randomIndex = empty[Math.floor(Math.random() * 
-        emptyCells.length)];
-
-    makeMove(randomIndex, "O");
-
-    if (gameActive) {
-        statusText.textContent = "Your turn";
+        this.classList.add("o");
     }
 }
 
 function checkWinner() {
-    for (let combo of winConditions) {
-        let a = combo[0];
-        let b = combo[1];
-        let c = combo[2];
+    let winner = false;
 
-        if (board[a] !=="" && 
+    winPatterns.forEach(patter => {
+        const [a,b,c] = patterns;
+
+        if (board[a] && 
             board[a] === board[b] && 
             board[a] === board[c] 
         ){
-        statusText.textContent =  `${board[a]} wins 🎉`;
+            winner = true;
+    }
+});
+
+    if (!board.includes("")) {
+        statusText.textContent = "It's a Draw!";
         gameActive = false;
         return;
-        }
     }
-}
-    if (!board.includes("")) {
-        statusText.textContent = "It's a draw!";
-        gameActive = false;
-    }
-
+    
 function resetGame() {
     board = ["", "", "", "", "", "", "","",""];
     gameActive = true;
